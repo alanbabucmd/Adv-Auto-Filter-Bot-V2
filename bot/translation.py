cfab36e7a7afccc570fa0c7ad84cbb7b995de2a6
alanbabucmd <84659625+alanbabucmd@users.noreply.github.com>
2021-06-10 13:10:29 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,10 +7,10 @@ class Translation(object):
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+created to work in AMIGO MOVIES 💫❣️
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+For More Movies..
+@AMIGOMOVIES2
 </i>"""    
     
     HELP_TEXT = """
@@ -43,12 +43,12 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@AMIGOMOVIES2
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/ALAN_007">ALAN_007</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
